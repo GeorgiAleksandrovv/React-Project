47479562c6fa3a7abd2f0bc6d3843c4bf8ca8c0b
GeorgiAleksandrovv <joro111work@gmail.com>
2023-12-01 16:14:52 +0200

    authGuard for addTrainer page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import { AuthProvider } from "./contexts/authContext";
 import Details from "./components/Details";
 import TrainerUpdate from "./components/trainerUpdate";
 import Logout from "./components/logout";
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
   return (
@@ -28,10 +29,15 @@ function App() {
           <Route path="/contactUs" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/addTrainer" element={<AddTrainer />} />
           <Route path="/trainers/:trainerId" element={<Details />} />
-          <Route path="/trainers/:trainerId/edit" element={<TrainerUpdate />} />
-          <Route path="/logout" element={<Logout />} />
+          <Route element={<AuthGuard />}>
+            <Route path="/addTrainer" element={<AddTrainer />} />
+            <Route
+              path="/trainers/:trainerId/edit"
+              element={<TrainerUpdate />}
+            />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
         </Routes>
         <Healthy />
         <Info />
